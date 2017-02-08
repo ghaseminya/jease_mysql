77609afa3f346c9b309ee61a25b425fc8841e68a
mghasemy <mohammad.ghasemy@gmail.com>
2017-02-08 13:08:05 +0330

    add travice

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Jease_mysql
+[![Build Status](https://travis-ci.org/mmghasemi/Jease_mysql.svg?branch=master)](https://travis-ci.org/mmghasemi/Jease_mysql)
+
 Create a SQL database vendor for jease
+
+Add MySql database to Jease
+
+we need change all jfix package to new package for work with a relational database system.
+
--- src/main/java/db/relational/RelationalDatabase.java
+++ src/main/java/db/relational/RelationalDatabase.java
@@ -1,11 +1,30 @@
 package db.relational;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 import java.util.List;
 
 /**
  * Created by mghasemy on 2/7/17.
  */
 public class RelationalDatabase {
+    public String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    final String DB_URL = "jdbc:mysql://localhost:3306/jease?useUnicode=true&characterEncoding=UTF-8";
+    Connection conn = null;
+    Statement stmt = null;
+    final String USER = "root";
+    final String PASS = "XXXX";
+
+    public RelationalDatabase() {
+        try {
+            Class.forName(JDBC_DRIVER);
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            stmt = conn.createStatement();
+        } catch (Exception s) {
+        }
+    }
+
     public <E> List<E> query(String TableN) {
         return null;
     }
@@ -16,6 +35,7 @@ public class RelationalDatabase {
         return null;
     }
     public <E> E queryUnique(String TableN, String Query) {
+
         return null;
     }
     public <E> boolean isUnique(String TableN, String Query) {
